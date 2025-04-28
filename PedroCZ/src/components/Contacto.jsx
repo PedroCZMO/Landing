ef43db0676524ff1de5b7d753223de276015bd66
PedroCZMO <drzpuerco@gmail.com>
2025-04-28 10:13:23 +0200

    Formulario-0.3

--- PedroCZ/src/components/Contacto.jsx
+++ PedroCZ/src/components/Contacto.jsx
@@ -5,9 +5,10 @@ export default function Contacto() {
   return (
   <section id="contacto" className="contacto">
   <h2>CONTACTO</h2>
-  <form action="https://formsubmit.co/pedroczmo23@gmail.com" method="POST">
+  <form action="https://getform.io/f/bkkyqwqb" method="POST">
     <input type="text" name="nombre" placeholder="Tu nombre" required />
     <input type="email" name="email" placeholder="Tu email" required />
+    <input type="hidden" name="_gotcha" style="display:none"/>
     <textarea name="mensaje" placeholder="¿Cómo puedo ayudarte?" required></textarea>
     <button type="submit">Enviar</button>
   </form>
